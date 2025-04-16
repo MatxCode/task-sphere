4c29a19e2cfc6f9007068ecf34fe78c601a910b5
mateo <letertre@et.esiea.fr>
2025-04-16 23:37:56 +0200

    creating SelectIssueType

--- src/Twig/Components/SelectIssueType.php
+++ src/Twig/Components/SelectIssueType.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Twig\Components;
+use App\Enum\IssueType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\ValidatableComponentTrait;
 
@@ -10,4 +13,22 @@ class SelectIssueType
 {
     use DefaultActionTrait;
     use ValidatableComponentTrait;
+
+    #[LiveProp]
+    public \App\Entity\Issue $issue;
+
+    #[LiveProp]
+    public array $types = [];
+
+    #[LiveProp(writable: true)]
+    public IssueType $type;
+
+    public function updateType(EntityManagerInterface $em): void
+    {
+        $this->validate();
+
+        $this->issue->setType($this->type);
+
+        $em->flush();
+    }
 }
