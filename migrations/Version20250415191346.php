4d48b6214010db9d1a6a47728fe1005d81579b73
mateo <letertre@et.esiea.fr>
2025-04-18 14:53:44 +0200

    fix database postgre bug

--- migrations/Version20250415191346.php
+++ migrations/Version20250415191346.php
@@ -21,13 +21,18 @@ final class Version20250415191346 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE issue ADD status SMALLINT NOT NULL');
-        $this->addSql('ALTER TABLE project CHANGE key_code key_code VARCHAR(5) NOT NULL');
+
+        // Syntaxe PostgreSQL pour modifier le type d'une colonne
+        $this->addSql('ALTER TABLE project ALTER COLUMN key_code TYPE VARCHAR(5)');
+        // Pour ajouter la contrainte NOT NULL (si elle n'existe pas déjà)
+        $this->addSql('ALTER TABLE project ALTER COLUMN key_code SET NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE project CHANGE key_code key_code VARCHAR(255) NOT NULL');
+        // Syntaxe PostgreSQL pour annuler les modifications
+        $this->addSql('ALTER TABLE project ALTER COLUMN key_code TYPE VARCHAR(255)');
         $this->addSql('ALTER TABLE issue DROP status');
     }
 }
