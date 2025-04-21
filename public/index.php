75184b93981e2f870851b1c40c0e53a9cead122a
mateo <letertre@et.esiea.fr>
2025-04-21 16:47:56 +0200

    fix .env loading

--- public/index.php
+++ public/index.php
@@ -2,6 +2,16 @@
 
 use App\Kernel;
 
+// Configuration forcée pour Railway
+$_ENV['APP_ENV'] = $_SERVER['APP_ENV'] = 'prod';
+$_ENV['APP_DEBUG'] = $_SERVER['APP_DEBUG'] = '0';
+putenv('APP_ENV=prod');
+
+// Désactivation du chargement .env si le fichier n'existe pas
+if (!file_exists(dirname(__DIR__).'/.env')) {
+    $_SERVER['SYMFONY_DOTENV_VARS'] = false;
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
